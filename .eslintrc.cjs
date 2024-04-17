0bd0e920a8fb86500e01848d33d6054c2da8f18c
lorain <lorainwings@gmail.com>
2024-04-17 19:58:17 +0800

    feat: init

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    // 防止'return \'string\''这种多重转义, 而是使用"return 'string'"
+    quotes: ['error', 'single', { avoidEscape: true }],
+    // semi: ['error', 'never'],
+    indent: 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/comma-dangle': 'off'
+  }
+}
